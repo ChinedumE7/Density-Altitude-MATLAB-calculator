fb5fae8fc5bbd04441704806dbd426965e8d5c34
ChinedumE7 <dumj.777@gmail.com>
2025-11-28 23:25:47 -0600

    Add files via upload
    If you want to run the density altitude calculator, you will need this user-defined function.

---
+++ Kelv_to_Celc.m
@@ -0,0 +1,6 @@
+function [Celcius] = Kelv_to_Celc(Kelvin)
+% Function to convert inputed temperature from Kelvin to dgerees Celcius.
+%[Celcius] = Kelv_to_Celc(Kelvin)
+Celcius = Kelvin - 273.15;
+end
+
